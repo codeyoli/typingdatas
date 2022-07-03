30a47bde77a1467df12778be88ee6b35ce221bd8
Nijat Muhtar <nijatmn@gmail.com>
2022-07-03 01:31:24 -0400

    latest code change

--- java/Program.java
+++ java/Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main(String[] args){
-        System.out.println("Hell World!");
+        System.out.println("Hello World!");
         System.out.println("Let's learn some coding!");
     }
 }
