eb9efa01dd0b5ef3af94d1ba9a6aafaebf41163f
Nijat Muhtar <nijatmn@gmail.com>
2022-07-07 21:09:45 -0400

    latest

---
+++ java/selenium/EelementLocator.java
@@ -0,0 +1,84 @@
+/*
+
+Example element:
+
+<a id='search' name='bar' class='query'> Click for google </a>
+
+ */
+
+
+// element location using CSS selector
+By location = By.cssSelector("a#search");
+By location = By.cssSelector("a#search");
+By location = By.cssSelector("a#search");
+By location = By.cssSelector("a#search");
+By location = By.cssSelector("a#search");
+By location = By.cssSelector("a#search");
+By location = By.cssSelector("a#search");
+
+// element location using the XPATH selector
+By location = By.xpath("//a");
+By location = By.xpath("//a");
+By location = By.xpath("//a");
+By location = By.xpath("//a");
+By location = By.xpath("//a");
+By location = By.xpath("//a");
+By location = By.xpath("//a");
+
+// element location using the link text
+By location = By.linkText("Click for google");
+By location = By.linkText("Click for google");
+By location = By.linkText("Click for google");
+By location = By.linkText("Click for google");
+By location = By.linkText("Click for google");
+By location = By.linkText("Click for google");
+By location = By.linkText("Click for google");
+
+// element location using the id attribute value
+By location = By.id("search");
+By location = By.id("search");
+By location = By.id("search");
+By location = By.id("search");
+By location = By.id("search");
+By location = By.id("search");
+By location = By.id("search");
+
+// element locaiton using the name attribute value
+// <a name='value'>text</a>
+By location = By.name("bar");
+By location = By.name("bar");
+By location = By.name("bar");
+By location = By.name("bar");
+By location = By.name("bar");
+By location = By.name("bar");
+By location = By.name("bar");
+
+// element location using the HTML element name
+// <a id='value'>text</a>
+By location = By.tagName("a");
+By location = By.tagName("a");
+By location = By.tagName("a");
+By location = By.tagName("a");
+By location = By.tagName("a");
+By location = By.tagName("a");
+By location = By.tagName("a");
+
+// element location using the partial link text
+By location = By.partialLinkText("google");
+By location = By.partialLinkText("google");
+By location = By.partialLinkText("google");
+By location = By.partialLinkText("google");
+By location = By.partialLinkText("google");
+By location = By.partialLinkText("google");
+By location = By.partialLinkText("google");
+
+// element location using the class attribute value
+By location = By.className("query");
+By location = By.className("query");
+By location = By.className("query");
+By location = By.className("query");
+By location = By.className("query");
+By location = By.className("query");
+By location = By.className("query");
+
+DONE
