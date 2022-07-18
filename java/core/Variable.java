c0bf5cb6e2f0cb4432d4638b331ddb7bebc9b6fc
Nijat Muhtar <nijatmn@gmail.com>
2022-07-17 22:48:03 -0400

    latest

---
+++ java/core/Variable.java
@@ -0,0 +1,27 @@
+
+public class Variables {
+
+    @Test
+    public void variableIntro() {
+        // Create varaibles
+        int age = 29;
+        double pi = 3.14;
+        char grade = 'c';
+        String test = "Regression";
+        boolean report = false;
+
+        // Update variable content
+        age = 30;
+        pi = 3.16;
+        grade = 'A';
+        test = "Smoke";
+        report = true;
+
+        // Using the variable
+        System.out.println(age);
+        System.out.println(pi);
+        System.out.println(grade);
+        System.out.println(test);
+        System.out.println(report);
+    }
+}
