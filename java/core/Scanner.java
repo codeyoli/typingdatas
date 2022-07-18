fe18a7684146faa1de43c989c99a7244f26ea583
Nijat Muhtar <nijatmn@gmail.com>
2022-07-17 23:11:32 -0400

    latest

---
+++ java/core/Scanner.java
@@ -0,0 +1,24 @@
+public static void main(String[]args) {
+    // 1. Create a scanner
+    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+
+    // 2. Get user inputs
+    int count = scan.nextInt();
+    double ratio = scan.nextDouble();
+    boolean report = scan.nextBoolean();
+    String testName = scan.nextLine();
+
+    // 3. Close the scanner
+    scan.close();
+    scan.close();
+    scan.close();
+    scan.close();
+}
+DONE
