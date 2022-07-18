9b7335e1ca4d11a4f51e6323815b805fbe66685f
Nijat Muhtar <nijatmn@gmail.com>
2022-07-17 23:02:40 -0400

    added console output

---
+++ java/core/ConsoleOutput.java
@@ -0,0 +1,30 @@
+public class Variables {
+
+    @Test
+    public void output(){
+        // prints in single line
+        System.out.print("Hello");
+        System.out.print("Hello");
+        System.out.print("Hello");
+        System.out.print("Hello");
+
+        // prints and goes to the new line using \n
+        System.out.print("Hello\n");
+        System.out.print("Hello\n");
+        System.out.print("Hello\n");
+        System.out.print("Hello\n");
+
+        // prints and goes back to start of the line using \r
+        System.out.print("Hello\r");
+        System.out.print("Hello\r");
+        System.out.print("Hello\r");
+        System.out.print("Hello\r");
+
+        // prints and goes to the new line
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
+DONE
