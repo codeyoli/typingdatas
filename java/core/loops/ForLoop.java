dccb7ca40b44cff64e1fcbbe502c9c7be75b1689
Nijat Muhtar <nijatmn@gmail.com>
2022-08-22 12:33:21 -0400

    added FOR LOOP

---
+++ java/core/loops/ForLoop.java
@@ -0,0 +1,51 @@
+// count up 0 -> 10
+for(int num = 0; num <= 10; num++){
+    System.out.println(num);
+}
+
+for(int num = 0; num <= 10; num++){
+    System.out.println(num);
+}
+
+for(int num = 0; num <= 10; num++){
+    System.out.println(num);
+}
+
+for(int num = 0; num <= 10; num++){
+    System.out.println(num);
+}
+
+
+// count down 10 -> 0
+for(int i = 10; i >= 0; i--) {
+    System.out.println(i);
+}
+
+for(int i = 10; i >= 0; i--) {
+    System.out.println(i);
+}
+
+for(int i = 10; i >= 0; i--) {
+    System.out.println(i);
+}
+
+for(int i = 10; i >= 0; i--) {
+    System.out.println(i);
+}
+
+// count up 10,20,30 ~ 90,100
+for(int n = 10; n <= 100; n+=10) {
+    System.out.println(n);
+}
+
+for(int n = 10; n <= 100; n+=10) {
+    System.out.println(n);
+}
+
+for(int n = 10; n <= 100; n+=10) {
+    System.out.println(n);
+}
+
+for(int n = 10; n <= 100; n+=10) {
+    System.out.println(n);
+}
